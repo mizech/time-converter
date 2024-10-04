a4914737d979bd1a1a72b3db08ae2d35bef586ff
mizech <michael.zech@hotmail.de>
2024-10-04 10:23:10 +0200

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Michael on 04.10.24.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
