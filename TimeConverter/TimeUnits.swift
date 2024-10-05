bff97dacb957d7a83c97e89c718c0d821c8203dd
mizech <michael.zech@hotmail.de>
2024-10-05 10:09:13 +0200

    Basic implementation

---
+++ TimeConverter/TimeUnits.swift
@@ -0,0 +1,8 @@
+//
+//  TimeUnits.swift
+//  TimeConverter
+//
+//  Created by Michael on 04.10.24.
+//
+
+import Foundation
---
+++ TimeConverter/UnitPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  UnitPickerView.swift
+//  TimeConverter
+//
+//  Created by Michael on 04.10.24.
+//
+
+import SwiftUI
+
+struct UnitPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnitPickerView()
+}
